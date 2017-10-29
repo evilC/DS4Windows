2e9087179eb335db5f1b68a5f06b7928c933952a
Clive Galway <evilc@evilc.com>
2017-10-29 16:23:35 +0000

    Add Test console app

---
+++ TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using DS4Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DS4Devices.findControllers();
+            IEnumerable<DS4Device> devices = DS4Devices.getDS4Controllers();
+            if (devices.Count() == 0)
+            {
+                Console.WriteLine("No device found");
+                return;
+            }
+            DS4Device device = devices.ElementAt(0);
+        }
+    }
+}
